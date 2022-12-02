5833cd62ab27fa2a2f6b0cb3dfc9439cdd5b8a9b
shahaf <shahaf1131@gmail.com>
2022-12-02 17:32:59 +0200

    remove irellevant host-permissions and run content-script. fix logic & memory leak

---
+++ content-script.js
@@ -0,0 +1,19 @@
+let intervalId = null
+
+intervalId = setInterval(addFocusListener, 1000)
+
+function addFocusListener() {
+   const elSearchBar = document.querySelector('#side .selectable-text')
+   if (!elSearchBar) return
+   clearInterval(intervalId)
+   intervalId = null
+   elSearchBar.addEventListener("input", focus)
+   window.onunload(() => { elSearchBar.removeEventListener('input', focus) })
+
+   function focus(ev) {
+      const text = ev.target.innerHTML
+      if (!text) setTimeout(() => {
+         elSearchBar.focus()
+      }, 1)
+   }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
-window.addEventListener('DOMContentLoaded', onAppMount)
-
+// window.addEventListener('DOMContentLoaded', onAppMount)
+onAppMount()
 let intervalId = null
 
 function onAppMount() {
@@ -14,6 +14,24 @@ function addFocusListener() {
    intervalId = null
    elSearchBar.addEventListener("input", function (ev) {
       const text = ev.target.innerHTML
-      if (!text) elSearchBar.focus()
+      if (!text) setTimeout(() => {
+         elSearchBar.focus()
+      }, 1)
    })
 }
+
+let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+chrome.scripting.executeScript({
+   target: { tabId: tab.id },
+   function: fillInputs,
+   args: [startingDay, endDay, clockInHour, clockOutHour]
+});
+
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+// chrome.scripting.executeScript({
+//    target: { tabId: tab.id },
+//    function: fillInputs,
+//    args: [startingDay, endDay, clockInHour, clockOutHour]
+// });
